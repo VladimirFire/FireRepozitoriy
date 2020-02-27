56bc6fd2d7676532056e11a4872bba8ea9b13970
Vladimir Cherednichenko <trenirovki81@mail.ru>
2020-02-27 14:28:48 +0300

    Zadanie 5 sdelano

---
+++ lesson05/filereadshort/filereadshort.go
@@ -0,0 +1,15 @@
+
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+)
+
+func main() {
+    bs, err := ioutil.ReadFile("filereadshort.go")
+    if err != nil {
+        return
+    }
+    fmt.Println(string(bs))
+}
